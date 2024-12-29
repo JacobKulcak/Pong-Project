fb1b154e5dc8f6819e9e9cf75366e272cddee957
Jacob Kulcak <144879268+JacobKulcak@users.noreply.github.com>
2024-12-29 00:33:37 -0500

    changed window icon and caption

--- Testing.py
+++ Testing.py
@@ -5,9 +5,10 @@ import sys
 pygame.init()
 
 # Set up the display
-width, height = 800, 600
+width, height = 1000, 800
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("My First Pygame Window")
+pygame.display.set_caption("BOB THE VIDEOGAME")
+pygame.display.set_icon(pygame.image.load('bob.png'))
 
 # Main game loop
 running = True
